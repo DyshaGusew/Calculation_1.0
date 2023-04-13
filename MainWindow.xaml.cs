de01047bac446c22af1f6cb42e2caebac3ec56f0
DyshaGusew <pav060210@gmail.com>
2023-04-13 21:58:38 +1000

    Рабочая версия Дополнительные кнопкки и их реализация

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace Calculation_1._0
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculation_1._0
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
@@ -31,7 +36,29 @@
 
                 case "=":
                     string value = new DataTable().Compute(textUp.Text, null).ToString();
+                    value = value.Replace(",",".");
                     textUp.Text = value;
+                    break;
+
+                case ",":
+                    textUp.Text += ".";
+                    break;
+
+                case "x²":
+                    value = new DataTable().Compute(textUp.Text, null).ToString();
+                    value = value.Replace(",", ".");
+                    textUp.Text = value;
+
+                    string val = Convert.ToString(Convert.ToDouble( textUp.Text) * Convert.ToDouble(textUp.Text));
+                    val = val.Replace(",", ".");
+                    textUp.Text = val;
+                    break;
+
+                case "«":
+                    if(textUp.Text.Length!= 0)
+                    {
+                        textUp.Text = textUp.Text.Remove(textUp.Text.Length - 1);
+                    }
                     break;
 
                 default:
